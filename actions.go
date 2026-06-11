fdc8a2d810b08f5614e768b029185a2545c80398
Ned <wned@proton.me>
2026-06-11 20:41:19 +0100

    Clear In flag for non-ready players when dealing next hand

--- actions.go
+++ actions.go
@@ -207,6 +207,7 @@ func deal(g *Game, pn uint, data uint) error {
 			} else {
 				g.players[i].Cards[0] = 0
 				g.players[i].Cards[1] = 0
+				g.players[i].In = false
 			}
 
 			g.players[i].Called = false
